0fbb792640472928aa01aebfbf01488ecc17784e
Elmurat <example@gmail.com>
2022-03-30 22:13:40 +0600

    fmewo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/provider"
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  <ChakraProvider>
+    return <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
