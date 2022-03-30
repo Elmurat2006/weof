ee1beb29cccb5e22b5d6882b51e9b440c6f5fc59
Elmurat <example@gmail.com>
2022-03-30 22:19:46 +0600

    few

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from "@chakra-ui/provider"
+import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  <ChakraProvider>
-    return <Component {...pageProps} />
-  </ChakraProvider>
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
